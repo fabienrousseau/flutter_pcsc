2814a2cf0e62fe408be4c92176b4b587c1872534
Fabien Rousseau <fabienrousseau@yahoo.fr>
2021-10-22 10:12:30 +0200

    Add comments to improve pub.dev score

--- flutter_pcsc_linux/lib/src/pcsc_linux.dart
+++ flutter_pcsc_linux/lib/src/pcsc_linux.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_pcsc_linux/src/pcsc_bindings.dart';
 import 'package:flutter_pcsc_platform_interface/flutter_pcsc_platform_interface.dart';
 
-
+/// The main class to use to deal with PCSC.
 class PcscLinux extends PcscPlatform {
   static void registerWith() {
     PcscPlatform.instance = PcscLinux();
@@ -17,22 +17,26 @@ class PcscLinux extends PcscPlatform {
    * which uses an Isolate to really be async
    */
 
+  /// Establishes a PCSC context.
   @override
   Future<int> establishContext(int scope) {
     return _binding.establishContext(scope);
   }
 
+  /// Lists available readers for this context.
   @override
   Future<List<String>> listReaders(int context) {
     return _binding.listReaders(context);
   }
 
+  /// Connects to the card using the specified reader.
   @override
   Future<Map> cardConnect(
       int context, String reader, int shareMode, int protocol) {
     return _binding.cardConnect(context, reader, shareMode, protocol);
   }
 
+  /// Transmits an APDU to the card.
   @override
   Future<List<int>> transmit(
       int hCard, int activeProtocol, List<int> commandBytes,
@@ -41,21 +45,29 @@ class PcscLinux extends PcscPlatform {
         newIsolate: newIsolate);
   }
 
+  /// Disconnects from the card.
   @override
   Future<void> cardDisconnect(int hCard, int disposition) {
     return _binding.cardDisconnect(hCard, disposition);
   }
 
+  /// Releases the PCSC context.
   @override
   Future<void> releaseContext(int context) {
     return _binding.releaseContext(context);
   }
 
+  /// Waits for a card to be present on the specified reader.
+  ///
+  /// If a card is already present, it does not wait.
   @override
   Future<Map> waitForCardPresent(int context, String readerName) {
     return _binding.waitForCardPresent(context, readerName);
   }
 
+  /// Waits for a card to be removed on the specified reader.
+  ///
+  /// If a card is already removed, it does not wait.
   @override
   Future<void> waitForCardRemoved(int context, String readerName) {
     return _binding.waitForCardRemoved(context, readerName);
--- flutter_pcsc_windows/lib/src/pcsc_windows.dart
+++ flutter_pcsc_windows/lib/src/pcsc_windows.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_pcsc_platform_interface/flutter_pcsc_platform_interface.dart';
 import 'package:flutter_pcsc_windows/src/pcsc_bindings.dart';
 
+/// The main class to use to deal with PCSC.
 class PcscWindows extends PcscPlatform {
   static void registerWith() {
     PcscPlatform.instance = PcscWindows();
@@ -11,22 +12,26 @@ class PcscWindows extends PcscPlatform {
   /*
    * Not really asynchronous (the C call is synchronous), but it will be easier to use if a Future is returned
    */
+  /// Establishes a PCSC context.
   @override
   Future<int> establishContext(int scope) {
     return _binding.establishContext(scope);
   }
 
+  /// Lists available readers for this context.
   @override
   Future<List<String>> listReaders(int context) {
     return _binding.listReaders(context);
   }
 
+  /// Connects to the card using the specified reader.
   @override
   Future<Map> cardConnect(
       int context, String reader, int shareMode, int protocol) {
     return _binding.cardConnect(context, reader, shareMode, protocol);
   }
 
+  /// Transmits an APDU to the card.
   @override
   Future<List<int>> transmit(
       int hCard, int activeProtocol, List<int> commandBytes,
@@ -35,21 +40,29 @@ class PcscWindows extends PcscPlatform {
         newIsolate: newIsolate);
   }
 
+  /// Disconnects from the card.
   @override
   Future<void> cardDisconnect(int hCard, int disposition) {
     return _binding.cardDisconnect(hCard, disposition);
   }
 
+  /// Releases the PCSC context.
   @override
   Future<void> releaseContext(int context) {
     return _binding.releaseContext(context);
   }
 
+  /// Waits for a card to be present on the specified reader.
+  ///
+  /// If a card is already present, it does not wait.
   @override
   Future<Map> waitForCardPresent(int context, String readerName) {
     return _binding.waitForCardPresent(context, readerName);
   }
 
+  /// Waits for a card to be removed on the specified reader.
+  ///
+  /// If a card is already removed, it does not wait.
   @override
   Future<void> waitForCardRemoved(int context, String readerName) {
     return _binding.waitForCardRemoved(context, readerName);
